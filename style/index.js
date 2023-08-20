8b972314cf5e7eff01a59b42e6dcd39eb512674e
anikmollik <anikmollik122321@gmail.com>
2023-08-21 02:07:50 +0600

    added js function

--- style/index.js
+++ style/index.js
@@ -1,4 +1,8 @@
 
-document.getElementById('card').addEventListener('click',function(){
+document.getElementById('card').addEventListener('click', function () {
+    const cardName = document.getElementById('card-name');
+    const cardNameText = cardName.innerText;
+    const cardNameTextAdded = document.getElementById('added-card-name');
     
-})
+    cardNameTextAdded.innerText = '1. ' + cardNameText;
+});
